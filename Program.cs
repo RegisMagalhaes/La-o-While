ce5168ee66ebb89751c01dae72501adfcebfee84
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-17 23:52:43 -0300

    Exercicio pergunta se você deseja receber um bom dia e mostra quantas vezes te comprimentou

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicios_com_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você deseja receber um bom dia?");
+            string resposta = Console.ReadLine();
+
+            int numeroDeVezes = 0;
+
+            while(resposta == "Sim") {
+                //Executado quando a condição é verdadeira
+                Console.WriteLine("Bom dia!");
+
+                numeroDeVezes++;
+
+                Console.WriteLine("Ainda deseja receber um bom dia?");
+                resposta = Console.ReadLine();
+            }
+            Console.WriteLine($"Numero de vezes: {numeroDeVezes}");
+            Console.WriteLine("Fim do programa");
+        }
+    }
+}
